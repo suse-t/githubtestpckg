1fa50e8088f7dc2ea843c1dbd40cbd2b19cd4c5a
suse-t <staut@bgc-jena.mpg.de>
2020-02-07 21:29:35 +0100

    test

---
+++ R/testfile.R
@@ -0,0 +1,13 @@
+#' Say Hello
+#'
+#'I coukd include some further information here.
+#'
+#' @param name string
+#'
+#' @return A string
+#' @export
+#'
+#' @examples
+helloworld<-function(name){
+  print(paste("Hello", name))
+}
